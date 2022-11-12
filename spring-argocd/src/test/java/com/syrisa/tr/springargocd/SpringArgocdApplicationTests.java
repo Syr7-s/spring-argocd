baab0d85d5b6166f87685a1cbc021ccacd033728
İsa Sayar <isa.sayar1725@gmail.com>
2022-11-12 13:27:02 +0300

    Initial Commit

---
+++ spring-argocd/src/test/java/com/syrisa/tr/springargocd/SpringArgocdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syrisa.tr.springargocd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringArgocdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
